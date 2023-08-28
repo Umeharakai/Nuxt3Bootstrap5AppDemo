21c1f48951c76f950df26e97e47213f40f156143
Umeharakai <104058586+Umeharakai@users.noreply.github.com>
2023-08-28 04:57:44 +0000

    cdd

--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,67 @@
 <template>
-  <div class="container text-center my-5">
-    <h1>{{ data.title }}</h1>
-    <div class="alert alert-primary" role="alert">{{ data.description }}</div>
-  </div>
-  <div class="container mb-5">
-    <div class="card mb-3 mx-auto">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img
-            src="https://img.cpcdn.com/recipes/3695776/894x1461s/03855e67f64548a05b1f995faabe8f57?u=12868943&p=1455726325"
-            class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">材料</h5>
-            <div class="card-text">
-              <ul>
-                <li v-for="item in data.material">{{ item.text }}</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container mb-5">
-    <h2>作り方</h2>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
-        <p>☆をボウルに混ぜ合わせておく。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
-        <p>別のボウルに★を全て混ぜ合わせておく。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">3</span></div>
-        <p>肉の厚みがある部分を切り開き、厚みを一定にする。<br />鶏肉１枚を２～３等分の大きめに切る。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">4</span></div>
-        <p>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">5</span></div>
-        <p>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。<br />
-          粉が余るようでしたら、二度付けをお勧めします！</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">6</span></div>
-        <p>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">7</span></div>
-        <p>油を160度に温める。<br />
-          (温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">8</span></div>
-        <p>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">9</span></div>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。
-      </div>
+  <div>
+    <!-- ナビゲーションバー -->
+    <nav class="bg-dark text-white">
+      <ul class="list-inline">
+        <li class="list-inline-item"><Nuxt-link to="/">財務会計アプリ</nuxt-link></li>
+        <li class="list-inline-item"><Nuxt-link to="/ImagePage">複数ページ</nuxt-link></li>
+      </ul>
+    </nav>
 
-    </div>
-    <ol>
+    <!-- アプリのUIコンポーネント -->
+    <p class="text-primary">残高</p>
+    <p>{{ state.balance }}</p>
+    <input type="number" v-model="state.transactionAmount" placeholder="金額を入力してください" class="form-control mt-2" />
+    <button @click="addTransaction" class="btn btn-primary mt-2">入金</button>
+    <button @click="withdrawTransaction" class="btn btn-primary mt-2">引き出し</button>
 
-    </ol>
-  </div>
-  <div class="container mb-5">
-    <div class="row gx-5">
-      <div class="col-sm">
-        <h3>コツ・ポイント</h3>
-
-        <p>揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</p>
-        <p>好みで粉類に粗びき黒胡椒小さじ1/2を足しても◎</p>
-        <p>にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</p>
-        <p>パプリカパウダーは無くても可</p>
-      </div>
-      <div class="col-sm">
-        <h3>このレシピの生い立ち</h3>
-
-        <p>今まで作ったKFCレシピは、卵液や粉もかなり余るし、何よりも
-          何かひと味足りない……と思って、やっとKFC味にたどり着きました！</p>
-        <p>どの家庭にもあるような材料で☆</p>
-        <p>牛乳と卵液、粉類も必要量のみになっているので無駄が無く経済的です☆</p>
-      </div>
-    </div>
+    <!-- 履歴 -->
+    <h2 class="text-primary mt-2">履歴</h2>
+    <ul>
+      <li v-for="transaction in state.transactionHistory" :key="transaction.id">
+        {{ transaction.description }} - {{ transaction.amount }}
+      </li>
+    </ul>
+    <Nuxt-link to="/ImagePage">次へ</Nuxt-link>
   </div>
 </template>
+
 <script setup>
- const data = useRecipe()
+import { reactive } from 'vue';
+import ImagePage from './ImagePage.vue';
+
+const state = reactive({
+  balance: 0,
+  transactionAmount: 0,
+  transactionHistory: [],
+});
+
+const addTransaction = () => {
+  // 入金処理
+  const transaction = {
+    id: Date.now(),
+    description: '入金',
+    amount: state.transactionAmount,
+  };
+  state.transactionHistory.push(transaction);
+  state.balance += state.transactionAmount;
+  state.transactionAmount = 0;
+};
+
+const withdrawTransaction = () => {
+  // 引き出し処理
+  if (state.transactionAmount > state.balance) {
+    alert('残高が不足しています');
+    return;
+  }
+
+  const transaction = {
+    id: Date.now(),
+    description: '引き出し',
+    amount: -state.transactionAmount, // 引き出しは負の値として表現する
+  };
+  state.transactionHistory.push(transaction);
+  state.balance -= state.transactionAmount;
+  state.transactionAmount = 0;
+};
 </script>
